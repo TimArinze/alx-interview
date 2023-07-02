f527cece94baeb6b6826aa7414bfdfb3ca289392
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-02 22:46:11 +0100

    task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,18 +6,18 @@ the Pascal's triangle of n
 
 
 def pascal_triangle(n):
-  """Pascals Triangle"""
-  if n <= 0:
-    return []
-  else:
-    triangle = []
-    for i in range(n):
-      row = []
-      for j in range(i+1):
-        if j == 0 or j == i:
-          row.append(1)
-        else:
-          prev_row = triangle[i - 1]
-          row.append(prev_row[j -1] + prev_row[j])
-      triangle.append(row)
-    return triangle
+    """Pascals Triangle"""
+    if n <= 0:
+        return []
+    else:
+        triangle = []
+        for i in range(n):
+            row = []
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    prev_row = triangle[i - 1]
+                    row.append(prev_row[j - 1] + prev_row[j])
+            triangle.append(row)
+        return triangle
