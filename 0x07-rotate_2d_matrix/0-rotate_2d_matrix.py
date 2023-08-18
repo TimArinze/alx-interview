a208507c2afff31e381d6146e29c383dce70914c
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-18 03:06:03 +0100

    task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,10 +8,14 @@ from typing import List
 def rotate_2d_matrix(matrix: List[List]):
     """Given an mxn = nxn 2D matrix, rotate it 90 degrees clockwise"""
     n = len(matrix)
-    #n is the n of the matrix
+    # n is the length of the row and column of the nxn matrix
     for i in range(n):
-        #m is the i-max of the matrix
+        # i transversing the column
         for j in range(i, n):
+            # j transversing the row
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            # first iteration column changes all the value for column to row
+        # for the second row we start with second column since we have settle
+        # first column and so on.
     for row in matrix:
         row.reverse()
