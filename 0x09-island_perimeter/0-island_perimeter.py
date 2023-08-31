43ab9802ca42c85fc4ca806dd502b3cdd496b41e
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-31 17:51:39 +0100

    task

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 
 """Island Perimeter"""
-from typing import List
 
 
-def island_perimeter(grid: List[List]) -> int:
+def island_perimeter(grid):
     """returns the parameter of the island described in the grid"""
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i + 1][j] == 0:
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+                if i == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
